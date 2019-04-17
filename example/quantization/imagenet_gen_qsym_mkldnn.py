956d861966f4aac3c1aa1f7da84782716643e680
xinyu <xinyu1.chen@intel.com>
2019-04-17 13:24:25 +0800

    add more models support

--- example/quantization/imagenet_gen_qsym_mkldnn.py
+++ example/quantization/imagenet_gen_qsym_mkldnn.py
@@ -92,21 +92,13 @@ def save_params(fname, arg_params, aux_params, logger=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate a calibrated quantized model from a FP32 model with Intel MKL-DNN support')
-    parser.add_argument('--model', type=str, choices=['resnet18_v1',
-                                                      'resnet50_v1',
-                                                      'resnet101_v1',
-                                                      'inceptionv3',
-                                                      'squeezenet1.0',
-                                                      'mobilenet1.0',
-                                                      'mobilenetv2_1.0',
-                                                      'imagenet1k-resnet-152',
-                                                      'imagenet1k-inception-bn',
-                                                      'custom'],
-                        help='currently only supports imagenet1k-resnet-50_v1, imagenet1k-resnet-152 or imagenet1k-inception-bn.'
-                             'you can set to custom to load your pre-trained model.')
-    parser.add_argument('--use-gluon-model', type=bool, default=False,
-                        help='If enabled, will download pretrained model from Gluon-CV '
-                             'and convert to symbolic model ')
+    parser.add_argument('--model', type=str, default='resnet50_v1',
+                        help='model to be quantized.')
+    parser.add_argument('--epoch', type=int, default=0,
+                        help='number of epochs, default is 0')
+    parser.add_argument('--use-pretrained', type=bool, default=True,
+                        help='If enabled, will download pretrained model from MXNet '
+                             'modelzoo or Gluon-CV and convert to symbolic model.')
     parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--label-name', type=str, default='softmax_label')
     parser.add_argument('--calib-dataset', type=str, default='data/val_256_q90.rec',
@@ -165,27 +157,24 @@ if __name__ == '__main__':
         download_calib_dataset('http://data.mxnet.io/data/val_256_q90.rec', args.calib_dataset)
 
     # download model
-    if args.model in ['resnet18_v1',
-                      'resnet50_v1',
-                      'resnet101_v1',
-                      'squeezenet1.0',
-                      'mobilenet1.0',
-                      'mobilenetv2_1.0',
-                      'inceptionv3']:
-        logger.info('model %s is converted from GluonCV' % args.model)
-        args.use_gluon_model = True
-    if args.use_gluon_model == True:
-        prefix = convert_from_gluon(model_name=args.model, image_shape=args.image_shape, classes=1000, logger=logger)
-        epoch = 0
-        sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
-    elif args.model == 'custom':
+    if args.use_pretrained == True:
+        logger.info('Get pre-trained model from MXNet or Gluoncv modelzoo.')
+        logger.info('if you want to use custom model, please set use_pretrained = False.')
+        if args.model in ['imagenet1k-resnet-152, imagenet1k-inception-bn']:
+            logger.info('model %s is downloaded from MXNet modelzoo' % args.model)
+            prefix, epoch = download_model(model_name=args.model, logger=logger)
+        else:
+            logger.info('model %s is converted from GluonCV' % args.model)
+            prefix = convert_from_gluon(model_name=args.model, image_shape=args.image_shape, classes=1000, logger=logger)
+            rgb_mean = '123.68,116.779,103.939'
+            rgb_std = '58.393, 57.12, 57.375'
+            epoch = 0
+    else:
         dir_path = os.path.dirname(os.path.realpath(__file__))
         prefix = os.path.join(dir_path, 'model', args.model)
-        epoch = 0
-        sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
-    else:
-        prefix, epoch = download_model(model_name=args.model, logger=logger)
-        sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
+        epoch = args.epoch
+
+    sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
 
     sym = sym.get_backend_symbol('MKLDNN_QUANTIZE')
 
@@ -224,45 +213,42 @@ if __name__ == '__main__':
         excluded_sym_names += ['flatten']
         if exclude_first_conv:
             excluded_sym_names += ['conv_1']
-    elif args.model in ['resnet18_v1', 'resnet50_v1', 'resnet101_v1']:
-        rgb_mean = '123.68,116.779,103.939'
-        rgb_std = '58.393, 57.12, 57.375'
+    elif args.model.find('resnet') != -1 and args.model.find('v1') != -1:
         if exclude_first_conv:
             excluded_sym_names += ['resnetv10_conv0_fwd']
-    elif args.model == 'squeezenet1.0':
-        rgb_mean = '123.68,116.779,103.939'
-        rgb_std = '58.393, 57.12, 57.375'
+    elif args.model.find('resnet') != -1 and args.model.find('v2') != -1:
+        excluded_sym_names += ['resnetv20_flatten0_flatten0']
+        if exclude_first_conv:
+            excluded_sym_names += ['resnetv20_conv0_fwd']
+    elif args.model.find('vgg') != -1:
+        if exclude_first_conv:
+            excluded_sym_names += ['vgg0_conv0_fwd']
+    elif args.model.find('squeezenet1') != -1:
         excluded_sym_names += ['squeezenet0_flatten0_flatten0']
         if exclude_first_conv:
             excluded_sym_names += ['squeezenet0_conv0_fwd']
-    elif args.model == 'mobilenet1.0':
-        rgb_mean = '123.68,116.779,103.939'
-        rgb_std = '58.393, 57.12, 57.375'
+    elif args.model.find('mobilenet') != -1 and args.model.find('v2') == -1:
         excluded_sym_names += ['mobilenet0_flatten0_flatten0',
                                'mobilenet0_pool0_fwd']
         if exclude_first_conv:
             excluded_sym_names += ['mobilenet0_conv0_fwd']
-    elif args.model == 'mobilenetv2_1.0':
-        rgb_mean = '123.68,116.779,103.939'
-        rgb_std = '58.393, 57.12, 57.375'
+    elif args.model.find('mobilenet') != -1 and args.model.find('v2') != -1:
         excluded_sym_names += ['mobilenetv20_output_flatten0_flatten0']
         if exclude_first_conv:
             excluded_sym_names += ['mobilenetv20_conv0_fwd']
     elif args.model == 'inceptionv3':
-        rgb_mean = '123.68,116.779,103.939'
-        rgb_std = '58.393, 57.12, 57.375'
         if exclude_first_conv:
             excluded_sym_names += ['inception30_conv0_fwd']
-    elif args.model == 'custom':
+    else:
+        logger.info('Set proper RGB configs for model %s' % args.model)
         # add rgb mean/std of your model.
         rgb_mean = '0,0,0'
         rgb_std = '0,0,0'
         # add layer names you donnot want to quantize.
+        logger.info('Set proper excluded_sym_names for model %s' % args.model)
         excluded_sym_names += ['layers']
         if exclude_first_conv:
             excluded_sym_names += ['layers']
-    else:
-        raise ValueError('model %s is not supported in this script' % args.model)
 
     label_name = args.label_name
     logger.info('label_name = %s' % label_name)
